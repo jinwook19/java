b83b4c994660bdabb5ac220936044cbb897be514
jinwook19 <drum0977@gmail.com>
2019-02-23 18:08:55 +0900

    자동차 속성으로 복습

--- src/test/CarClass.java
+++ src/test/CarClass.java
@@ -1,5 +1,26 @@
 package test;
 
 public class CarClass {
+	// 1. 차의 속성 4가지 차색상 , 차크기, 차모델 명, 차무게
+	String color;
+	String size;
+	String modelname;
+	int weight;
+	
 
+
+	public CarClass() {
+	}
+
+
+
+	public CarClass(String color, String size, String modelname, int weight) {
+		this.color = color;
+		this.size = size;
+		this.modelname = modelname;
+		this.weight = weight;
+	}
+	
+	
+	
 }
--- src/test/CarMain.java
+++ src/test/CarMain.java
@@ -1,10 +1,35 @@
 package test;
 
+import practice.PersonClass;
+
 public class CarMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//1. 선언,생성,초기화
+		// 선언
+		CarClass car1;
+		// 생성
+		car1 = new CarClass();
+		// 초기화
+		car1.color="파란색";
+		car1.modelname="그랜저";
+		car1.size="대형";
+		car1.weight=2000;
+		
+		System.out.println(car1.color + car1.modelname + car1.size + car1.weight);
+		//2. 선언과 생성, 초기화
+		//선언과 생성
+		CarClass car2 = new CarClass();
+		//초기화
+		car2.color="파란색";
+		car2.modelname="그랜저";
+		car2.weight=3000;
+		
+		System.out.println(car2.color);
+		//3. 선언과 생성과 초기화
+		CarClass car3 = new CarClass("빨간색","대형","그랜저",2000);
+		System.out.println(car3.color);
 	}
+
 
 }
